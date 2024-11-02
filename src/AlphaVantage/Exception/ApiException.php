af14368362812945c0b8b8e6533ce212e231b743
sukhrobnuraliev <sukhrobnuralievv@gmail.com>
2024-11-02 16:05:10 +0500

    Introduced custom exception classes - ApiException

---
+++ src/AlphaVantage/Exception/ApiException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace AlphaVantage\Exception;
+
+/**
+ * Exception thrown when the Alpha Vantage API returns an error or invalid response.
+ */
+class ApiException extends AlphaVantageException {}
